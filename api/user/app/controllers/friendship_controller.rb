52d597628e925c8db2d2ff07a2212958d298c284
dxjenkins-x <shannon.dunn@utc.com>
2020-02-06 20:17:16 -0500

    stuff

--- api/user/app/controllers/friendship_controller.rb
+++ api/user/app/controllers/friendship_controller.rb
@@ -2,7 +2,7 @@ class FriendshipController < ApplicationController
     include Response
     # GET /users
     def index
-      @friends = User.find(params[:id]).friends
+      @friends = User.find(params[:user_id]).friends
       json_response(@friends)
     end
   
